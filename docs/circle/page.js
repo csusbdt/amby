66a3e80b472cb93d74397ef2c89b4e31b7a674ce
David Turner <csusbdt@gmail.com>
2023-07-15 20:25:23 -1000

    X

--- docs/circle/page.js
+++ docs/circle/page.js
@@ -10,16 +10,6 @@ const bin   = bf * Math.pow(PHI, -7);
 const dur   = 1000;
 const group = new c_start_group();
 
-const n1 = 12;
-const s_1 = new c_seq(dur * 1, [ 
-	bf * p(2, n1, 0), 
-	bf * p(2, n1, 2), 
-	bf * p(2, n1, 5), 
-	bf * p(2, n1, 4), 
-], bin, 1);
-
-
-
 const start_audio = _ => {
 	window.start_audio = null;
 	window.stop_audio  = stop_audio;
@@ -35,9 +25,9 @@ const stop_audio = _ => {
 let img = n => new c_img("./bubble/images/" + n + ".png");
 
 const borders       = img("borders");
-const back          = img("back"   );
-const volume        = img("volume" );
-const audio_blue    = img("audio"  );
+const back          = img("back");
+const volume        = img("volume");
+const audio_blue    = img("audio");
 const audio_yellow  = audio_blue.clone_yellow();
 
 const audio = new c_toggle(audio_blue, audio_yellow, null, start_audio, stop_audio);
@@ -46,20 +36,17 @@ const speed  = 100;
 const BLUE   = 0;
 const YELLOW = 1;
 
-function c_obj(n, on_yellow = null, on_blue = null) {
-	this.on_yellow = on_yellow;
-	this.on_blue   = on_blue;
-	this.border    = img(n + "_border");
-	this.blue      = img(n + "_blue"  );
-	this.yellow    = img(n + "_blue").clone_yellow();
-	this.borders   = [];
-	this.blues     = [];
-	this.yellows   = [];
+function c_obj(n) {
+	this.border  = img(n + "_border");
+	this.blue    = img(n + "_blue  ");
+	this.yellow  = img(n + "_yellow");
+	this.borders = [];
+	this.blues   = [];
+	this.yellows = [];
 	for (let i = 0; i < 3; ++i) {
 		this.borders.push(img(n + "_border_" + i));
 		this.blues  .push(img(n + "_blue_"   + i));
-		this.yellows.push(img(n + "_blue_"   + i).clone_yellow());
-//		this.yellows.push(this.blues[i].clone_yellow());
+		this.yellows.push(blues[i].clone_yellow());
 	}	
 	this.state   = BLUE;
 	this.i       = null;
@@ -70,18 +57,16 @@ c_obj.prototype.draw = function() {
 	if (this.state === BLUE) {
 		this.blue.draw();
 		if (this.i !== null) {
-			this.yellows[this.i].draw();
+			this.blues[this.i].draw();
 			this.borders[this.i].draw();
 		}
 	} else {
 		this.yellow.draw();
 		if (this.i !== null) {
-			this.blue.draw();
 			this.yellows[this.i].draw();
 			this.borders[this.i].draw();
 		}
 	}
-	this.border.draw();
 };
 
 c_obj.prototype.next = function() {
@@ -90,7 +75,6 @@ c_obj.prototype.next = function() {
 			this.i     = null;
 			this.id    = null;
 			this.state = YELLOW;
-			if (this.on_yellow !== null) this.on_yellow();
 		} else {
 			this.id = setTimeout(this.next.bind(this), speed);
 		}
@@ -99,7 +83,6 @@ c_obj.prototype.next = function() {
 			this.i     = null;
 			this.id    = null;
 			this.state = BLUE;
-			if (this.on_blue !== null) this.on_blue();
 		} else {
 			this.id = setTimeout(this.next.bind(this), speed);
 		}
@@ -112,24 +95,21 @@ c_obj.prototype.click = function() {
 		if (this.state === BLUE) {
 			this.i = 0; 
 		} else {
-			this.i = this.blues.length - 1;
+			this.i = this.blue.length - 1;
 		}
 		this.id = setTimeout(this.next.bind(this), speed);
 		return true;
 	} else return false;
 };
 
-const center_on_yellow = _ => group.add(s_1);
-const center_on_blue   = _ => group.remove(s_1);
-
-const objs = [ new c_obj("big", center_on_yellow, center_on_blue) ];
+const objs = [ new c_obj("") ];
 
 const draw_objs = _ => {
-	draw(objs);
+	draw_bubble();
 };
 
 const click_objs = _ => {
-	return click(objs);
+	return click_bubble();
 };
 
 let start_external_audio = null;
