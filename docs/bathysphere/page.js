c4f5b77d23f220e2c068cebddb684892de61f411
David Turner <csusbdt@gmail.com>
2023-07-26 09:50:51 -1000

    X

--- docs/bathysphere/page.js
+++ docs/bathysphere/page.js
@@ -37,20 +37,22 @@ const s5 = new c_seq(dur * 16, [
 	bf * p(2, 23, 28 + 23),
 ], bin, 1);
 
-const g1 = [ s1, s2 ];
-const g2 = [ s1, s2, s3 ];
+//const g1 = [ s1, s2 ];
+//const g2 = [ s1, s2, s3 ];
 const g3 = [ s1, s2, s3, s4, s5 ];
 
 const start_audio = _ => {
 	window.start_audio = null;
 	window.stop_audio  = stop_audio;
-	start(start_group);
+//	start(start_group);
+	start(g3);
 };
 
 const stop_audio = _ => {
 	window.start_audio = start_audio;
 	window.stop_audio  = null;
-	stop(start_group);
+//	stop(start_group);
+	stop(g3);
 };
 
 let img = n => new c_img("./bathysphere/images/" + n + ".png");
@@ -60,89 +62,121 @@ const back          = img("back"         );
 const volume_border = img("volume_border");
 const volume        = img("volume"       );
 
-const BLUE      = 0;
-const YELLOW    = 1;
+// const BLUE      = 0;
+// const YELLOW    = 1;
 
 const photo   = img("photo");
 const green   = img("green");
-const borders = Array(3).fill(null).map((_, i) => img("border_" + i));
-const blues   = Array(3).fill(null).map((_, i) => img("blue_"   + i));
-const yellows = blues.map(o => o.clone_yellow());
-const colors  = Array(3).fill(BLUE);
+// const borders = Array(3).fill(null).map((_, i) => img("border_" + i));
+// const blues   = Array(3).fill(null).map((_, i) => img("blue_"   + i));
+// const yellows = blues.map(o => o.clone_yellow());
+// const colors  = Array(3).fill(BLUE);
+const cover_border = img("border_0");
+const cover_blue   = img("blue_0");
 
-let state = 0;
+//let state = 0;
 
-const draw_objs = _ => {
-	draw(photo);
-	draw(green);
-	draw(borders[0]);
-	if (state === 0) {
-		draw(blues[0]); 
-		draw(borders[0]);
-	} else if (state === 1) {
-		draw(blues[1]); 
-		draw(borders[1]);
-	} else if (state === 2) {
-		draw(blues[2]); 
-		draw(borders[2]);
-	}
-};
+// const draw_objs = _ => {
+// 	draw(photo);
+// 	draw(green);
+// 	draw(borders[0]);
+// 	if (state === 0) {
+// 		draw(blues[0]); 
+// 		draw(borders[0]);
+// 	} else if (state === 1) {
+// 		draw(blues[1]); 
+// 		draw(borders[1]);
+// 	} else if (state === 2) {
+// 		draw(blues[2]); 
+// 		draw(borders[2]);
+// 	}
+// };
 
-const click_objs = _ => {
-	if (state === 0 && click(blues[0])) {
-		state = 1;
-		start_group.set(g1);
-		start_audio();
-		return true;
-	} else if (state === 1 && click(blues[1])) {
-		state = 2;
-		start_group.set(g2);
-		return true;
-	} else if (state === 1 && click(blues[0])) {
-		state = 0;
-		start_group.remove_all();
-		stop_audio();
-		return true;
-	} else if (state === 2 && click(blues[2])) {
-		state = 3;
-		start_group.set(g3);
-		return true;
-	} else if (state === 2 && click(blues[0])) {
-		state = 1;
-		start_group.set(g1);
-		return true;
-	} else if (state === 3 && click(blues[0])) {
-		start_group.set(g2);
-		state = 2;
-		return true;
-	}
-	return false;
-};
+// const click_objs = _ => {
+// 	if (state === 0 && click(blues[0])) {
+// 		state = 1;
+// 		start_group.set(g1);
+// 		start_audio();
+// 		return true;
+// 	} else if (state === 1 && click(blues[1])) {
+// 		state = 2;
+// 		start_group.set(g2);
+// 		return true;
+// 	} else if (state === 1 && click(blues[0])) {
+// 		state = 0;
+// 		start_group.remove_all();
+// 		stop_audio();
+// 		return true;
+// 	} else if (state === 2 && click(blues[2])) {
+// 		state = 3;
+// 		start_group.set(g3);
+// 		return true;
+// 	} else if (state === 2 && click(blues[0])) {
+// 		state = 1;
+// 		start_group.set(g1);
+// 		return true;
+// 	} else if (state === 3 && click(blues[0])) {
+// 		start_group.set(g2);
+// 		state = 2;
+// 		return true;
+// 	}
+// 	return false;
+// };
 
 let start_external_audio = null;
 
 const click_page = _ => {
-    if (click(back)) return run_page("home"); 
-	else start_external_audio = null;
-    if (click(volume)) run_volume();
-	if (click_objs()) on_resize();
+    if (click(back)) {
+		stop_audio();
+		return run_page("home"); 
+	}
+    if (click(volume)) return run_volume();
+	if (click(cover_blue)) {
+		if (window.stop_audio === null) {
+			start_audio();
+		} else {
+			stop_audio();
+		}
+	}
+	
+// 	if (window.stop_audio === null) {
+// 	    if (click(back)) return run_page("home"); 
+// //		else start_external_audio = null;
+// 		start_audio();
+// 	} else {
+// 		stop_audio();
+// 	}
+	on_resize();
+//	if (click_objs()) on_resize();
 };
 
 const draw_page = _ => {
 	draw(bg_green);
-	draw_objs();
-    draw(back);
-    draw(volume);
-    draw(back_border);
-    draw(volume_border);
+	if (window.stop_audio === null) {
+	    draw(back);
+	    draw(volume);
+	    draw(back_border);
+	    draw(volume_border);
+		draw(cover_blue);
+		draw(cover_border);
+	} else {
+		draw(photo);
+		draw(green);
+		draw(cover_border);
+	    draw(back);
+	    draw(volume);
+	    draw(back_border);
+	    draw(volume_border);
+	}
 };
 
 const run = _ => {
-    if (window.stop_audio !== null && window.stop_audio !== stop_audio) {
-		window.stop_audio();
-		start_external_audio = window.start_audio;
-	}
-	if (state !== 0) start_audio();
+	if (window.stop_audio !== null) window.stop_audio();
+ //    if (window.stop_audio !== null && window.stop_audio !== stop_audio) {
+	// 	window.stop_audio();
+	// 	start_external_audio = window.start_audio;
+	// }
+//	if (state !== 0) start_audio();
     on_resize = draw_page;
     on_click  = click_page;
     on_resize();
